ebf299405fe13932346a7922f12fa552e0015e3c
TerevaM <terevamillet@yahoo.fr>
2023-10-21 02:18:23 +0200

    navbar done

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -1,21 +1,112 @@
 import "@/css/navbar.css";
-
+import Image from "next/image";
+import Prime from "../../public/assets/svg/prime.svg";
+import TwitchLogo from "@/pictures/ahouioui.png";
 export default function Navbar() {
   return (
     <>
       <nav>
         <div id="left-content">
-          <div>logo</div>
+          <Image src={TwitchLogo} alt="twitch logo" />
           <ul>
             <li>Suivis</li>
             <li>Parcourir</li>
+            <li className="hide-icon">
+              <svg
+                type="color-fill-current"
+                width="20px"
+                height="20px"
+                version="1.1"
+                viewBox="0 0 20 20"
+                x="0px"
+                y="0px"
+              >
+                <g>
+                  <path
+                    fill-rule="evenodd"
+                    d="M9.171 4.171A4 4 0 006.343 3H6a4 4 0 00-4 4v.343a4 4 0 001.172 2.829L10 17l6.828-6.828A4 4 0 0018 7.343V7a4 4 0 00-4-4h-.343a4 4 0 00-2.829 1.172L10 5l-.829-.829zm.829 10l5.414-5.414A2 2 0 0016 7.343V7a2 2 0 00-2-2h-.343a2 2 0 00-1.414.586L10 7.828 7.757 5.586A2 2 0 006.343 5H6a2 2 0 00-2 2v.343a2 2 0 00.586 1.414L10 14.172z"
+                    clip-rule="evenodd"
+                  ></path>
+                </g>
+              </svg>
+            </li>
+            <li className="hide-icon">
+              <svg
+                type="color-fill-current"
+                width="20px"
+                height="20px"
+                version="1.1"
+                viewBox="0 0 20 20"
+                x="0px"
+                y="0px"
+              >
+                <g>
+                  <path d="M5 2a2 2 0 00-2 2v8a2 2 0 002 2V4h8a2 2 0 00-2-2H5z"></path>
+                  <path
+                    fill-rule="evenodd"
+                    d="M7 8a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H9a2 2 0 01-2-2V8zm2 0h6v8H9V8z"
+                    clip-rule="evenodd"
+                  ></path>
+                </g>
+              </svg>
+            </li>
+            <li id="others">
+              <svg
+                width="100%"
+                height="100%"
+                viewBox="0 0 20 20"
+                focusable="false"
+                aria-hidden="true"
+              >
+                <path d="M10 18a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0-6a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM8 4a2 2 0 1 0 4 0 2 2 0 0 0-4 0z"></path>
+              </svg>
+            </li>
           </ul>
-          <div>others</div>
         </div>
         <div id="search">
-          <h5>Recherche</h5>
+          <label>
+            <svg
+              width="100%"
+              height="100%"
+              version="1.1"
+              viewBox="0 0 20 20"
+              x="0px"
+              y="0px"
+              data-a-selector="tw-core-button-icon"
+            >
+              <g>
+                <path
+                  fill-rule="evenodd"
+                  d="M13.192 14.606a7 7 0 111.414-1.414l3.101 3.1-1.414 1.415-3.1-3.1zM14 9A5 5 0 114 9a5 5 0 0110 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </g>
+            </svg>
+          </label>
+          <input type="text" placeholder="Rechercher" />
         </div>
         <ul id="right-content">
+          <li className="hide-icon">
+            <a href="#">
+              <svg
+                width="100%"
+                height="100%"
+                version="1.1"
+                viewBox="0 0 20 20"
+                x="0px"
+                y="0px"
+                data-a-selector="tw-core-button-icon"
+              >
+                <g>
+                  <path
+                    fill-rule="evenodd"
+                    d="M13.192 14.606a7 7 0 111.414-1.414l3.101 3.1-1.414 1.415-3.1-3.1zM14 9A5 5 0 114 9a5 5 0 0110 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </g>
+              </svg>
+            </a>
+          </li>
           <li>
             <a href="#">
               <svg
